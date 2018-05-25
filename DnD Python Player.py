475aa5b08c4daebbdfeac5d7852167ff0725579c
JoJo0259 <36492916+JoJo0259@users.noreply.github.com>
2018-05-24 21:25:15 -0700

    Added the rest of the abilities/modifiers

--- DnD Python Player.py
+++ DnD Python Player.py
@@ -6,9 +6,37 @@ Dice = ('D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100')
 class Player:
     Health = 0
     Strength = 0
+    Dexterity = 0
     Constitution = 0
-    def Start(Health):
+    Intelligence = 0
+    Wisdom = 0
+    Charisma = 0
+    SM = 0
+    DM = 0
+    CoM = 0
+    IM = 0
+    WM = 0
+    ChM = 0
+    def Start(Health, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma, SM, DM, CoM, IM, WM, ChM):
         Player.Health = int(input("What is your Health? : "))
+        Player.Strength = int(input("What is your Strength? : "))
+        Player.Dexterity = int(input("What is your Dexterity? : "))
+        Player.Constitution = int(input("What is your Constitution? : "))
+        Player.Intelligence = int(input("What is your Intelligence? : "))
+        Player.Wisdom = int(input("What is your Widom? : "))
+        Player.Charisma = int(input("What is your Charisma? : "))
+        Player.SM = Player.Strength - 10
+        Player.SM = Player.SM / 2
+        Player.DM = Player.Dexterity - 10
+        Player.DM = Player.DM / 2
+        Player.CoM = Player.Constitution - 10
+        Player.CoM = Player.CoM / 2
+        Player.WM = Player.Wisdom - 10
+        Player.WM = Player.WM / 2
+        Player.IM = Player.Intelligence - 10
+        Player.IM = Player.IM / 2
+        Player.ChM = Player.Charisma - 10
+        Player.ChM = Player.ChM / 2
     def __init__(self, Health):
         self.Health = Player.Health
 
